54b9b7bcd16f9eb6ce59f149c616a4608e1331d6
kmichaelk <130953568+kmichaelk@users.noreply.github.com>
2024-02-24 00:38:18 +0300

    Version 1.0.1 (2)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "io.github.kmichaelk.unnandroid"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         resourceConfigurations.clear()
         resourceConfigurations.add("ru")
