53396bae2f0a687ee9ba0dbc9db7d5fceba6e846
kmichaelk <130953568+kmichaelk@users.noreply.github.com>
2024-04-19 21:27:54 +0300

    Add license

--- app/src/main/java/io/github/kmichaelk/unnandroid/di/PersistenceModule.kt
+++ app/src/main/java/io/github/kmichaelk/unnandroid/di/PersistenceModule.kt
@@ -1,3 +1,20 @@
+/*
+ * This file is part of UNN-Android.
+ *
+ * UNN-Android is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * UNN-Android is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with UNN-Android.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package io.github.kmichaelk.unnandroid.di
 
 import android.content.Context
